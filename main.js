4a73a1057fec2099a2215a6244a1a2ffbea85431
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-11 14:29:34 -0600

    finish the project

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-var height 
+	// function when push the button 
+	function height(height,space){
+		var height = document.getElementById("height").value;
+ 		var character = document.getElementById("character").value;
+ 		var tree="";
+ 		var space = " " ;
+
+ 	for ( var i = 0 ; i< height; i++ ){  
+ 			
+ 		// add space 
+ 		for ( var k = 0 ; k <( height - i + 1); k++){
+ 		tree += (space );
+ 		}
+
+ 		// loop to add character
+ 		for (var j = 0 ; j < ((2 * i) + 1) ; j++){
+ 		tree +=  (character );
+ 			}
+ 			//adding new line
+ 			tree +=	'\n'
+	 	
+	 	}
+	 		console.log(tree);
+	 	}
+	 
+//event for clicking the button
+
+document.getElementById("build").addEventListener("click", height);
 
 
 
-var tree=['*','**','***','***','****','*****','******','*******'];
-for (var i=0; i< tree.length; ++i){
-	 tree[i]= tree[i];
-	 tree[i]+= " "
-	console.log(tree[i]);
-	// document.write (tree[i]);
-}
 
-// function for the height 
-	function (){
- 	height = document.getElementById("height").value;
- 	console.log(height);
-
-	}
