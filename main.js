32ed45ad7ad5dbafff801f7feb257eef12f61edb
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-11 11:21:25 -0600

    adding the html file and some js

---
+++ main.js
@@ -0,0 +1,18 @@
+var height 
+
+
+
+var tree=['*','**','***','***','****','*****','******','*******'];
+for (var i=0; i< tree.length; ++i){
+	 tree[i]= tree[i];
+	 tree[i]+= " "
+	console.log(tree[i]);
+	// document.write (tree[i]);
+}
+
+// function for the height 
+	function (){
+ 	height = document.getElementById("height").value;
+ 	console.log(height);
+
+	}
